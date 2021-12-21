4fc6de36c5ead191c0265034d63eb71c52fedef3
Abylai <aby.beken@aues.kz>
2021-12-21 22:17:37 +0600

    slide-up  kosyldy

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
+// slide-up script
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+    // removing smooth scroll on slide-up button click
+    $('html').css("scrollBehavior", "auto");
+});
