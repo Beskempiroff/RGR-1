21c6751227908ca925eba63ae47b6dc6e769a545
Abylai <aby.beken@aues.kz>
2021-12-21 22:19:25 +0600

    navbar-menu  kosyldy

--- script.js
+++ script.js
@@ -20,3 +20,7 @@ $('.scroll-up-btn').click(function(){
     // removing smooth scroll on slide-up button click
     $('html').css("scrollBehavior", "auto");
 });
+$('.navbar .menu li a').click(function(){
+    // applying again smooth scroll on menu items click
+    $('html').css("scrollBehavior", "smooth");
+});
